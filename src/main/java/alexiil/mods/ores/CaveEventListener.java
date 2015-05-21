0bb196ed284beee945a5fc23f78fe7b8e49c5bf1
AlexIIL <alexj9@me.com>
2015-05-21 20:52:32 +0100

    Update a few more things

--- src/main/java/alexiil/mods/ores/CaveEventListener.java
+++ src/main/java/alexiil/mods/ores/CaveEventListener.java
@@ -1,5 +1,7 @@
 package alexiil.mods.ores;
 
+import java.util.List;
+
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
@@ -7,6 +9,10 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.event.terraingen.OreGenEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.google.common.collect.Lists;
+
+import alexiil.mods.lib.SearchUtils;
 
 public class CaveEventListener {
     public static final CaveEventListener INSTANCE = new CaveEventListener();
@@ -16,6 +22,8 @@ public class CaveEventListener {
         World world = event.world;
         BlockPos eventPos = event.pos;
         Chunk chunk = world.getChunkFromBlockCoords(eventPos);
+
+        List<BlockPos> okPositions = Lists.newArrayList();
 
         for (BlockPos pos : SearchUtils.searchChunk(chunk)) {
             IBlockState b = world.getBlockState(pos);
